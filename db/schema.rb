70f08840c0d23c420b844dca5add6aa8bc0b4df3
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-19 13:47:13 -0700

    email is added to form and migration

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_09_19_005157) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
