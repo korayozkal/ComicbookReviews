8a7303fb100ecf4efa80762d9f83efc1d6acf9e6
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-18 23:52:16 -0700

    user controller actions update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :comicbooks, through: :reviews 
-    #has_secure_password 
+    has_secure_password 
     #validates_presence_of :username, uniquness: true 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  
+get '/signup' => 'users#new'
+post '/signup' => 'users#create'
+  
   resources :reviews
   resources :users
   resources :comicbooks
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
