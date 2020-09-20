5fddb423ef15014d373afbd7c51b31749e6a18a1
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-20 14:16:07 -0700

    User problem solved sessions and users controller updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,23 @@
 class SessionsController < ApplicationController
 
-    def destroy
+    def create
+        #binding.pry
+       # @user = User.find_by(username: params[:username])
+       @user = User.find_by(username: params[:user][:username])
+       if @user && @user.authenticate(params[:user][:password])
+        #if @user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect_to @user
+        
+        else
+          flash[:message] = "Incorrect login info, please try again"
+          render :new
+        end
+      end
+
+      def destroy
         session.clear
         redirect_to root_path
-    end
-
-    #def destroy
-        #User.find(session[:user_id]).destroy      
-        #session[:user_id] = nil         
-        #redirect_to root_path 
-    #end  
+      end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,26 +5,29 @@ class UsersController < ApplicationController
     end
 
     def create
-      @user = User.new(user_params)
-      if @user.save
-      redirect_to @user 
-      session[:user_id] = @user.id #we are logging in
-      else
-        render :new
+        #binding.pry
+        @user = User.new(user_params)
+        if @user.save
+        session[:user_id] = @user.id
+        redirect_to @user 
+        #session[:user_id] = @user.id #we are logging in
+        else
+          render :new
+        end
       end
-    end
-    
+
     def show
-       # @user = User.find(params[:id])
-        #Or I can use
-        @user = User.find_by_id(params[:id]) #this will bring nil
-        redirect_to '/' if !@user   #we are directing to home if it is not a user
-    end
+        #@user = User.find(params[:id])
+         #Or I can use
+         @user = User.find_by_id(params[:id]) #this will bring nil
+         #redirect_to '/' if !@user   #we are directing to home if it is not a user
+     end
+
     
-   
 private
-    def user_params
-        params.require(:user).permit(:username, :password)
-    end
+
+def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
 end
