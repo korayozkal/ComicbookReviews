a5eb7c86f9c8b5ebae602e06b43b3b789cdb21d6
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-20 22:11:37 -0700

    comicbook views and  controller new,create,show,index actions

--- app/controllers/comicbooks_controller.rb
+++ app/controllers/comicbooks_controller.rb
@@ -1,2 +1,34 @@
 class ComicbooksController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def new
+        @comicbook = Comicbook.new
+    end
+
+    def create
+        #binding.pry
+        #@comicbook = Comicbook.new(comicbook_params)
+        @comicbook = current_user.comicbooks.build(comicbook_params)
+        if @comicbook.save
+           redirect_to @comicbook
+        else
+           render :new
+        end
+        
+    end 
+
+    def show
+        @comicbook = Comicbook.find(params[:id])
+    end
+   
+    def index 
+        @comicbooks= Comicbook.all 
+    end 
+
+
+    private
+    def comicbook_params
+        params.require(:comicbook).permit(:title, :number, :writer, :artist, :publisher)
+    end
+
 end
