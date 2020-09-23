1e758613d6c52cf112b3d5b8f2503839d23bc577
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-23 12:27:52 -0700

    errors partial is added to layout

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   belongs_to :comicbook
 
   #validates_presence_of :rating, :comments
+  #validates :comicbook, uniqueness: {scope: :user_id, message: "may only have one review/rating per user"}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
     has_many :comicbooks
     has_many :reviews
     has_many :reviewed_comicbooks, -> {distinct}, through: :reviews, source: :comicbook  
+    
     has_secure_password 
+    validates_presence_of :username, :email, uniquness: true
 
-#validates_presence_of :username, uniquness: true, {case_sensitive: false} 
-#validates :password, length: {in : 4..72}
 end 
